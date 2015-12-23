681cdd040d8596c604d4373bcc7a6d85380c1d32
Alida Dev <alainduboissales@gmail.com>
2015-12-23 00:18:44 +0000

    Fixed stripe

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,16 +1,16 @@
-class Users::RegistrationController < Devise::RegistrationController
-
-   def create
+class Users::RegistrationsController < Devise::RegistrationsController
+  before_filter :select_plan, only: :new
+  
+  def create
     super do |resource|
-        if params[:plan]
-            resource.plan.id = params[:plan]
-            if resource.plan.id == 2
-                resource.save_with_payment
-            else
-                resource.save
-            end
+      if params[:plan]
+        resource.plan_id = params[:plan]
+        if resource.plan_id == 2
+          resource.save_with_payment
+        else
+          resource.save
         end
-     end
-   end
-
-end
+      end
+    end
+  end
+end  
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {registration: 'users/registration'}
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   devise_for :models
   resources :contacts
   get  '/about' => 'pages#about'
